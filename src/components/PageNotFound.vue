81f7679aae38465d334ded7b24f19d1a2649b49f
nagashi_ma_w <nagashima5.wataru@gmail.com>
2019-02-15 14:56:36 +0900

    TopページをHelloWorldからTopに書き換え、404ページを追加

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="wrapper">
+<main>
+<h1> ページが見つかりません </h1>
+</main>
+</div>
+</template>
+<script>
+export default {
+title: 'Not Found',
+description: ''
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Top from '@/components/Top'
+import PageNotFound from '@/components/PageNotFound'
 
 Vue.use(Router)
 
@@ -8,8 +9,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Top',
+      component: Top
+    },
+    {
+      path: '*',
+      name: 'Not Found',
+      component: PageNotFound,
+      title: 'Not Found'
     }
   ]
 })
